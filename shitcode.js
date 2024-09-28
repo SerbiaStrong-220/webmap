4434c78343f0abf8aea05abc08cfdaf3cb14bac9
UrPrice <107646133+UrPrice@users.noreply.github.com>
2024-09-28 13:36:39 +0300

    DonutsAdd (#50)

--- shitcode.js
+++ shitcode.js
@@ -15,6 +15,7 @@ const maps = {
 //  nox: {image: "nox.webp", name: "Nox"},
   axioma: {image: "axioma.webp", name: "Axioma"},
   eclipse: {image: "eclipse.webp", name: "Eclipse"},
+  donuts: {image: "donuts.webp", name: "Donuts"},
 //  centcomm: {image: "centcomm.webp", name:"CentComm"},
 //  avrite: {image: "avrite.webp", name: "Avrite"},
 //  marathon: {image: "marathon.webp", name: "Marathon"},
